6b1dd83995a9be1b3993c1808cb627f5c8e47443
Saurav Panda <113649785+dearlipu@users.noreply.github.com>
2026-04-03 11:54:09 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,17 @@
 export const siteConfig = {
   name: "Saurav Panda",
   title: "We design visuals that make brands impossible to ignore.",
-  description: "Agency website of Saurav Panda",
+  description: "A full-service creative agency delivering impactful design, cinematic video, motion graphics, branding, and digital solutions. From magazines and posters to social media content and business systems — we help ideas stand out.",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "sauravpanda34+web@gmail.com",
+    instagram: "https://www.instagram.com/dearlipu",
+    twitter: "https://x.com/dearlipu",
+    youtube: "https://www.youtube.com/@dearlipu",
   },
   aboutMe:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
-  skills: ["Javascript", "React", "Node.js", "Python", "AWS", "Docker"],
+    "we specialize in transforming ideas into powerful visuals and systems. We craft designs for magazines, books, posters, thumbnails, and social media posts. Our expertise extends to photo manipulation, motion graphics, cinematic video production, and content tailored for platforms like YouTube and Instagram. Beyond creative media, we also support businesses with data management, brand creation, and system development — from Excel and Word to PowerPoint and Access. With mastery in tools like Photoshop, Illustrator, Procreate, InDesign, Figma, Premiere Pro, After Effects, Blender, and many more we deliver professional results that blend creativity with precision. Whether you need a striking brand identity, engaging social content, or streamlined business solutions, we’re here to make it happen.",
+  skills: ["Publishing", "Graphic Design", "Video & Motion", "Social Media", "Creative Media", "Business Systems", "Branding"],
   projects: [
     {
       name: "AI Dev Roundup Newsletter",
@@ -97,6 +97,56 @@ export const siteConfig = {
         "Workflow systems",
         "Automation-ready structures"
       ],
+    },
+  ],
+  Badges: [
+    {
+      name: "Digital Marketing",
+      provider: "HubSpot",
+      year: 2025,
+      url: "https://app-na2.hubspot.com/academy/achievements/1v45tlhb/en/1/saurav-panda/digital-marketing"
+    },
+    {
+      name: "AI-Powered Ads Performance",
+      provider: "Google",
+      year: 2025,
+      url: "https://skillshop.credential.net/5b6c62f2-4f8e-4f82-8bdb-8cd9aefeb3c7#acc.WX8Ff6bA"
+    },
+    {
+      name: "Social Media Marketing",
+      provider: "HubSpot",
+      year: 2025,
+      url: "https://app-na2.hubspot.com/academy/achievements/0f4y823h/en/1/saurav-panda/social-media-marketing"
+    },
+    {
+      name: "Google Analytics",
+      provider: "Google",
+      year: 2025,
+      url: "https://skillshop.credential.net/506ca29b-8c3f-4dea-b5ea-6c3027dac521#acc.i5mY2yI4"
+    },
+    {
+      name: "Ethical Hacking",
+      provider: "LinkedIn",
+      year: 2023,
+      url: "https://www.linkedin.com/learning/certificates/492f3d853428ab8c07aa823953ae13173cda54bc1ae4b8413a2e34eb21709737?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BHB4vaxhZTMWVLWrF%2F0dHqw%3D%3D"
+    },
+    {
+      name: "Graphic Designing",
+      provider: "LinkedIn",
+      year: 2023,
+      url: "https://www.linkedin.com/learning/certificates/59cc927bed1f46dfb9dbe7783abd1f17985dcfafa3f3c6caa3663ed236796357?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BHB4vaxhZTMWVLWrF%2F0dHqw%3D%3D" 
+    },
+    {
+      name: "Robotic Automation (RPA)",
+      provider: "LinkedIn",
+      year: 2023,
+      url: "https://www.linkedin.com/learning/certificates/ea160e3235ab9771c6cbd7ef00f5d0d19fd59edd3b3097ad33d46e06ba360974?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BHB4vaxhZTMWVLWrF%2F0dHqw%3D%3D" 
+    },
+    {
+      name: "Business Analysis (CBAP) Prep",
+      provider: "LinkedIn",
+      year: 2023,
+      url: "https://www.linkedin.com/learning/certificates/0a42aed187459f9f5a6f0a4d1a2340669b6df76da0f22f3385e69c5390c1b93d" 
     },
   ]
 };
