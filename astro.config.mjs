a7604ac595d0fbae6c007833c8cb31be36ce3e7b
Saurav Panda <113649785+dearlipu@users.noreply.github.com>
2026-04-07 23:30:47 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  export default {
+  site: 'https://dearlipu.github.io',
+  integrations: [sitemap()],
+};
   vite: {
     plugins: [tailwindcss()],
   },
